40d2c44ecb126d027354aa63cd987fad12db4a2e
Benoit-Hayet <hayetbenoit@gmail.com>
2023-11-22 19:39:54 +0100

    Titre et mise en page

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <div className='nav'>
+           <a href='/' className='site-title'>My Fake Shop</a>
+        </div>
+    );
+};
+
+export default Navbar;
